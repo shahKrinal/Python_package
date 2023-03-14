a40c6103f8d812b8ca7cfa3cc98a0c0f23e200f5
Your Name <krinal.pysquad@gmail.com>
2023-03-14 17:40:15 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythontrack',
+    version='1.1',
+    packages=['test'],
+    url='https://github.com/shahKrinal/Python_package/tree/main',
+    license='',
+    author='krinal',
+    author_email='shahkrinal877@gmail.com',
+    description='Creating python package'
+)
